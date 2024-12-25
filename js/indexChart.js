24de17c8ffdea63eff4932684e469de5a3bce6c7
Nasir Abbasian <nasir77.a@gmail.com>
2024-12-25 01:57:48 -0800

    Add user chart

---
+++ js/indexChart.js
@@ -0,0 +1,20 @@
+const ctx = document.getElementById('userChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: 'تعداد حاضرین',
+      data: [1,2,3,4,5,6],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
