f18678e6dd806338d764d564146c23f9ef1cad2c
Nasir Abbasian <nasir77.a@gmail.com>
2024-12-30 00:14:15 -0800

    Config chart js for salaries and presentation of persoals

--- js/indexChart.js
+++ js/indexChart.js
@@ -6,8 +6,9 @@ new Chart(ctx, {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [{
       label: 'تعداد حاضرین',
-      data: [1,2,3,4,5,6],
-      borderWidth: 1
+      data: [1, 2, 3, 4, 5, 6],
+      borderWidth: 1,
+      
     }]
   },
   options: {
@@ -18,3 +19,33 @@ new Chart(ctx, {
     }
   }
 });
+
+
+
+const payChart = document.getElementById('payChart');
+const payConfig = {
+  type: 'line',
+  data: {
+    labels: ["فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور"],
+    datasets: [{
+      label: "میانگین حقوق پرداختی",
+      data: [20, 18, 19, 23, 17, 20],
+      tension: 0.3,
+      fill:true
+    }, {
+      label: "حداقل حقوق پرداختی",
+      data: [18, 15, 18, 18, 9, 19.5],
+      tension: 0.3
+    }]
+  },
+  options: {
+    plugins: {
+      legend: {
+        labels: {
+          font: { family:"Vazirmatn RD FD" }
+        }
+      },
+    }
+  }
+}
+new Chart(payChart, payConfig);
