6d8bee0cc67d1610957615f7efc59ae2401f0110
Nasir Abbasian <nasir77.a@gmail.com>
2024-12-31 12:07:00 -0800

    Translate Data tables structure

--- js/personalsTable.js
+++ js/personalsTable.js
@@ -1,1 +1,24 @@
-let table = new DataTable('#personals_table');
+let table = new DataTable('#personals_table', {
+    language: {
+        processing: "در حال پردازش",
+        search: "جستجو:",
+        lengthMenu: "نمایش _MENU_ آیتم",
+        info: "در حال نمایش آیتم _START_ تا _END_  از _TOTAL_ آیتم",
+        infoEmpty: "هیچ آیتمی منطبق بر جست و جوی شما یافت نشد",
+        infoFiltered: "«جست و جو شده در بین _MAX_ آیتم»",
+        infoPostFix: "",
+        loadingRecords: "در حال بارگذاری اطلاعات...",
+        zeroRecords: "رکوردی یافت نشد",
+        emptyTable: "محتوایی وجود ندارد",
+        paginate: {
+            first: "اول",
+            previous: "قبلی",
+            next: "بعدی",
+            last: "آخرین"
+        },
+        aria: {
+            sortAscending: ": activer pour trier la colonne par ordre croissant",
+            sortDescending: ": activer pour trier la colonne par ordre décroissant"
+        }
+    }
+});
