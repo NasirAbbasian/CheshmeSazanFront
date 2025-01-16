bb39c38f2a37564a47cc7775c86c95af6e0636ed
Nasir Abbasian <nasir77.a@gmail.com>
2025-01-16 11:58:52 +0330

    Request with axios

---
+++ js/login.js
@@ -0,0 +1,30 @@
+// const axios = require('axios');
+var msg = document.getElementById("message")
+
+function loginCheck(){
+    axios.get("http://localhost/api/auth/check.php")
+     .then( res =>{
+        console.log("OK")
+        console.log(res)
+        msg.classList.add("bg-green-200","border-green-600","text-green-900")
+        msg.innerHTML = res.data.message
+     })
+     .catch(err=>{
+        console.log("Error")
+        console.log(err.response.data.message)
+        msg.classList.add("bg-red-200","border-red-600","text-red-900")
+        msg.innerHTML=err.response.data.message
+     })
+     .finally(()=>{
+        msg.classList.remove("hidden")
+     })
+}
+
+function login (){
+    var user = document.getElementById("username").value
+    var pass = document.getElementById("password").value
+    axios.get(`http://localhost/api/auth/login.php?username=${user}&password=${pass}`)
+         .finally(()=>{
+            loginCheck()
+         })
+}
